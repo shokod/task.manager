cc90b1d84b95537a7bdc97b1b7180b3656ebd699
shokod <delvinshoko@gmail.com>
2023-06-28 13:29:59 +0200

    Feature!Create & Update Projects

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,6 +26,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tasks',TaskController::class);
+    Route::apiResource('projects',ProjectController::class);
+
 });
 
 
